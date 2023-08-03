78b835ce49e2713819961eed463b4ea72ba92562
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2023-08-03 07:05:49 +0100

    update: added content

--- src/component/About/index.tsx
+++ src/component/About/index.tsx
@@ -20,6 +20,24 @@ export const About = () => {
                         <p className="text-big">
                             About
                         </p>
+                        <div className="flex flex-col justify-start">
+                            <p className="font-freckle text-4xl leading-10">
+                                📢 Introducing Duckcoin (Duck BSC) - The Ultimate Payment Meme Coin!
+                            </p>
+                            <p className="">Join the Duckies worldwide in embracing the people's cryptocurrency, Duckcoin! As a decentralized, open-source peer-to-peer digital currency, Duckcoin embodies the friendly, curious, and intelligent nature of Duckies, creating a vibrant DeFi community. <br /> <br />
+
+                                Experience a world of entertainment and wealth by joining the Duckie's community today! Duckcoin offers incredible utilities that will leave you amazed:   <br />
+                                🌟 Duckswap: Dive into the ultimate decentralized exchange (DEX) with staking, farming, and a cutting-edge Crosschain bridge. Say goodbye to high fees and security concerns—Duckswap leverages Layerzero and Axelar infrastructure for seamless and safe crosschain movement of assets. <br />
+
+                                🌟 Lottery: Get your quackers ready for thrilling lotteries, offering you a chance to win amazing prizes and make your wallet soar.  <br />
+
+                                🌟 Payments: Simplify your transactions with Duckcoin's payment system, allowing you to send and receive funds effortlessly.  <br />
+
+                                🌟 Wallets and Games: Explore a range of secure wallets and engaging games designed to enhance your Duckcoin experience. 💥The NFT ticketing platform brings huge usecase for NFTs holders around the 🌎 world.<br /><br />
+
+                                The Duckswap Bridge is live and waiting for you! <a href="https://duckcoin.xyz">https://duckcoin.xyz/</a>
+                            </p>
+                        </div>
                         <div className="flex flex-col justify-start items-start gap-5 pb-4">
                             <h2 className="font-freckle w-[328px] md:w-[540px]">
                                 New token in town. Fair presale, Fair tax. $Duck Memes rule.
