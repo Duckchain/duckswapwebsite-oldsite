a6c245633ede00e32d413fb115916f400b823fbf
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2023-08-03 06:34:43 +0100

    update: modified text

--- src/component/Bridge/Select.tsx
+++ src/component/Bridge/Select.tsx
@@ -3,7 +3,7 @@ import { StargateWidget } from "./Stargate"
 export const BridgeSelect = () => {
     return (
         <div className="text-center" id="bridge">
-            <h3 className="font-freckle text-3xl">Bridge</h3>
+            <h3 className="font-freckle text-3xl">Bridge with LayerZero</h3>
             <p className="mb-4">transfer your assets from one blockchain network to another.</p>
             <StargateWidget />
         </div>
