585580aa8f145226cc2a38b1f8453630aa0a31b7
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2023-09-09 09:38:31 +0100

    update: added countdown and message

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,27 @@
-import { About } from "./component/About"
-import { Duckmap } from "./component/Duckmap"
-import { Ducknomics } from "./component/Ducknomics"
-import { Footer } from "./component/Footer"
-import { Hero } from "./component/Hero"
-import { Buy } from "./component/HowtoBuy"
-import { JoinUs } from "./component/Joinus"
-import { NavbarMob } from "./component/NavbarMob"
-import { Utilities } from "./component/Utilities"
-import { BridgeSelect } from "./component/Bridge/Select"
+// import { About } from "./component/About"
+// import { Duckmap } from "./component/Duckmap"
+// import { Ducknomics } from "./component/Ducknomics"
+// import { Footer } from "./component/Footer"
+// import { Buy } from "./component/HowtoBuy"
+// import { JoinUs } from "./component/Joinus"
+// import { Utilities } from "./component/Utilities"
+// import { BridgeSelect } from "./component/Bridge/Select"
+// import { Hero } from "./component/Hero"
+// import { NavbarMob } from "./component/NavbarMob"
+import { CountDown } from './component/Countdown'
 
 function App() {
 
   return (
     <>
-      <Hero />
-      <About />
-      <Buy />
-      <Ducknomics />
-      <Utilities />
-      <BridgeSelect />
-      <Duckmap />
-      <JoinUs />
-      <Footer />
-      <NavbarMob />
+      <div className="mt-[10rem]">
+        <div className="text-center space-y-4">
+          <h1 className='text-5xl font-bold'>Coming Soon!!!</h1>
+          <p>New website release</p>
+          <p>High performance, scalable and low fees layer -2 solution for BNB Chain ⛓️ Decentralization, security for global adoption.</p>
+        </div>
+        <CountDown />
+      </div>
     </>
   )
 }
